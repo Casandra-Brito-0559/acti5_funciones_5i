f88dda32f34d296e379f08fe085582fb239c76e3
Casandra Brito Gomez <164533984+Casandra-Brito-0559@users.noreply.github.com>
2024-09-09 13:02:02 -0600

    Add files via upload

---
+++ funcionesv2.py
@@ -0,0 +1,38 @@
+print("Funciones creadas por el usuario")
+# las funciones
+def Mi_lista():
+    amigos=["Homero","Paty","Lety"]
+    for io in amigos:
+        print(io)
+# Tuplas
+        
+def Mi_tupla():
+    amigos=("Melany","Santi","Adri")
+    for lol in amigos:
+        print(lol)
+# Conjunto
+
+def Mi_conjunto():
+    amigos={"Karen","Zyanya","Rob"}
+    for misa in amigos:
+        print(misa)
+# Diccionario
+
+def Mi_diccionario():
+    Noc={
+    "Grupo":"NMIX",
+        "Miembro":"Wemo chec",
+        "Posicion":"no c"
+    }
+    for x in Noc:
+        print(Noc[x])
+# llamar a las funciones
+print("*-------LISTA------*")   
+Mi_lista()
+print("*-------TUPLA------*")     
+Mi_tupla()
+print("*-------CONJUNTO------*")     
+Mi_conjunto()
+print("*-------DICCIONARIO------*")     
+Mi_diccionario()
+print("*-------------*")     
